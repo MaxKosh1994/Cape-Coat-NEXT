acf5687e6ff5c90fec32ef50fa57bb6bb5062185
Alena-bo <bodnarchuk_alena@mail.ru>
2023-08-09 21:57:19 +0300

    seedsTest done

--- server/db/migrations/20230712111156-create-item.js
+++ server/db/migrations/20230712111156-create-item.js
@@ -9,7 +9,7 @@ module.exports = {
         type: Sequelize.INTEGER,
       },
       article: {
-        type: Sequelize.INTEGER,
+        type: Sequelize.STRING,
         allowNull: false,
       },
       name: {
