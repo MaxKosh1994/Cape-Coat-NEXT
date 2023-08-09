51566baf22c1ec111500326ff590a663d620c494
VasilyevPavel <iam100500@ya.ru>
2023-08-09 21:44:23 +0300

    navbar

---
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import IconButton from '@mui/material/IconButton';
+import Link from 'next/link';
+import { useTheme } from '@mui/material/styles';
+import { useMediaQuery } from '@mui/material';
+
+interface SearchBarProps {}
+
+const SearchBar: React.FC<SearchBarProps> = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+  return (
+    <>
+      <Link href="/search" passHref>
+        <IconButton
+          size="large"
+          aria-label="search"
+          color="inherit"
+          sx={{
+            padding: isMobile ? '3px' : '8px',
+          }}
+        >
+          <SearchIcon />
+        </IconButton>
+      </Link>
+    </>
+  );
+};
+
+export default SearchBar;
