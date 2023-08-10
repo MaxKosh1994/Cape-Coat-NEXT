9a6cdb82cac212553b26f4c4d71b100877a19c98
Max Kosheutov <maxkosh1994@cmail.com>
2023-08-10 21:29:36 +0300

    add all components in Account-Page

---
+++ client/src/components/accComp/profile/Profile.tsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from 'react';
+import styles from './Profile.module.css';
+import { Button } from '@mui/material';
+import ModalEditInfo from '../modalEditInfo/ModalEditInfo';
+import { IUserInfo } from './UserDataTypes';
+import fetchUserInfo from './FetchUserInfo';
+
+export default function Profile() {
+  const [open, setOpen] = useState<boolean>(false);
+  const [userInfo, setUserInfo] = useState<IUserInfo | undefined>(undefined);
+
+  useEffect(() => {
+    const response = fetchUserInfo();
+    response.then((data) => {
+      if (data && Object.keys(data).length > 0) {
+        setUserInfo(data);
+      }
+    });
+  }, []);
+
+  const handleOpen = () => setOpen(true);
+
+  return (
+    <div className={styles.mainDiv}>
+      <div className={styles.headerDiv}>
+        <h2>Личные данные</h2>
+      </div>
+      <div className={styles.mainContainer}>
+        <div className={styles.container}>
+          <div className={styles.liContainer}>
+            <h5 className={styles.headerFive}>Электронная почта</h5>
+            <div className={styles.pContainer}>
+              <p className={styles.pFive}>{userInfo?.email}</p>
+            </div>
+          </div>
+          <div className={styles.liContainer}>
+            <h5 className={styles.headerFive}>ФИО</h5>
+            <p className={styles.pFive}>{userInfo?.full_name}</p>
+          </div>
+          <div className={styles.liContainer}>
+            <h5 className={styles.headerFive}>Адрес</h5>
+            <div className={styles.pContainer}>
+              <p className={styles.pFive}>{userInfo?.address}</p>
+            </div>
+          </div>
+          <div className={styles.liContainer}>
+            <h5 className={styles.headerFive}>Номер телефона</h5>
+            <div className={styles.pContainer}>
+              <p className={styles.pFive}>{userInfo?.phone}</p>
+            </div>
+          </div>
+          <div className={styles.liContainer}>
+            <h5 className={styles.headerFive}>Ваш Telegram</h5>
+            <div className={styles.pContainer}>
+              <p className={styles.pFive}>{userInfo?.telegram}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.containerButton}>
+        <Button
+          className={styles.button}
+          onClick={handleOpen}
+          variant='contained'
+          color='primary'
+          type='submit'
+          style={{
+            backgroundColor: 'black',
+            color: 'white',
+          }}
+        >
+          Редактировать
+        </Button>
+      </div>
+      <ModalEditInfo
+        open={open}
+        setOpen={setOpen}
+        userInfo={userInfo}
+        setUserInfo={setUserInfo}
+      />
+    </div>
+  );
+}
---
+++ client/src/components/accComp/sureModal/SureModal.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import styles from './SureModal.module.css';
+import { signOutUserThunk } from '../../../app/thunkActionsAuth';
+import { useDispatch } from 'react-redux';
+import { checkSession } from '../../../app/sessionSlice';
+
+export default function SureModal({ open, setOpen }: ISureModal) {
+  const dispatch = useDispatch();
+  const [info, setInfo] = useState('');
+
+  const logOutHandler = async (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    try {
+      await dispatch(signOutUserThunk());
+      setInfo('Вы вышли из аккаунта');
+      setOpen(false);
+      dispatch(checkSession(false));
+    } catch (Error) {
+      console.log(Error);
+    }
+  };
+
+  return (
+    <Modal
+      open={open}
+      onClose={() => setOpen(false)}
+      aria-labelledby='modal-modal-title'
+      aria-describedby='modal-modal-description'
+    >
+      <Box className={styles.box}>
+        <div className={styles.mainContainer}>
+          {info.length > 0 && <p>{info}</p>}
+          <div className={styles.questionContainer}>
+            <h3>Вы уверены, что хотите выйти из аккаунта?</h3>
+          </div>
+          <div className={styles.buttonContainer}>
+            <Button
+              onClick={() => setOpen(false)}
+              className={styles.button}
+              type='submit'
+              variant='contained'
+              color='secondary'
+            >
+              Отмена
+            </Button>
+            <Button
+              onClick={logOutHandler}
+              className={styles.button}
+              type='submit'
+              variant='contained'
+              color='primary'
+            >
+              Выйти
+            </Button>
+          </div>
+        </div>
+      </Box>
+    </Modal>
+  );
+}
