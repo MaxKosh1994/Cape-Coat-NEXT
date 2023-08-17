defca5ff9ea27bdd2172f74047f0219218b3e86a
Max Kosheutov <maxkosh1994@cmail.com>
2023-08-17 17:37:47 +0300

    add navFAQcomp

---
+++ client/src/pages/FAQ/orderFAQ.tsx
@@ -0,0 +1,11 @@
+import NavFAQComp from '@/components/navFAQComp/NavFAQComp';
+import React from 'react';
+
+export default function orderFAQ() {
+  return (
+    <>
+      <NavFAQComp />
+      <div>orderFAQ</div>
+    </>
+  );
+}
