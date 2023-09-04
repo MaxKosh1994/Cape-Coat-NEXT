4984536a26abb11c847f22dfc6cac146ab5ad45c
Max Kosheutov <maxkosh1994@cmail.com>
2023-09-04 16:26:39 +0300

    Защитил страницы на фронте и часть роутов на бэке

--- client/src/pages/admin/content.tsx
+++ client/src/pages/admin/content.tsx
@@ -10,6 +10,9 @@ import MatModal from '../../components/Admin/Modal/MatModal/MatModal';
 import ItemTable from '../../components/Admin/ItemTable/ItemTable';
 import InfoModal from '../../components/Admin/InfoModal';
 import { dataAxios } from '../../components/Admin/HTTP/adminApi';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/app/store';
+import { useRouter } from 'next/router';
 
 import { Button } from '@mui/material';
 
@@ -38,70 +41,96 @@ export default function addContent() {
     dataAxios(setContent, setMessage, address);
   }, []);
 
+  const isAdmin = useSelector((state: RootState) => state.sessionSlice.isAdmin);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isAdmin) {
+      router.push('/signin');
+    }
+  }, [isAdmin]);
+
   return (
     <>
-      <NavAdminComp />
-      <div className={styles.routesDiv}>
-        <Button
-          className={styles.button}
-          onClick={handleOpenItem}
-          type='button'
-        >
-          Добавить товар
-        </Button>
-        <Button className={styles.button} onClick={handleOpenCat} type='button'>
-          Добавить категорию
-        </Button>
-        <Button className={styles.button} onClick={handleOpenCol} type='button'>
-          Добавить коллекцию
-        </Button>
-        <Button className={styles.button} onClick={handleOpenMat} type='button'>
-          Добавить материал
-        </Button>
-      </div>
-      <ItemTable
-        content={content}
-        setContent={setContent}
-        message={message}
-        setMessage={setMessage}
-        handleOpenItem={handleOpenItem}
-        setId={setId}
-      />
-      <ItemModal
-        openChange={openItem}
-        setOpenChange={setOpenItem}
-        id={id}
-        setContent={setContent}
-        open={open}
-        setOpen={setOpen}
-        message={message}
-        setMessage={setMessage}
-      />
-      <MatModal
-        openChange={openMat}
-        setOpenChange={setOpenMat}
-        open={open}
-        setOpen={setOpen}
-        message={message}
-        setMessage={setMessage}
-      />
-      <CatModal
-        openChange={openCat}
-        setOpenChange={setOpenCat}
-        open={open}
-        setOpen={setOpen}
-        message={message}
-        setMessage={setMessage}
-      />
-      <ColModal
-        openChange={openCol}
-        setOpenChange={setOpenCol}
-        open={open}
-        setOpen={setOpen}
-        message={message}
-        setMessage={setMessage}
-      />
-      <InfoModal info={message} open={open} setOpen={setOpen} />
+      {isAdmin && (
+        <>
+          <NavAdminComp />
+          <div className={styles.routesDiv}>
+            <Button
+              className={styles.button}
+              onClick={handleOpenItem}
+              type='button'
+            >
+              Добавить товар
+            </Button>
+            <Button
+              className={styles.button}
+              onClick={handleOpenCat}
+              type='button'
+            >
+              Добавить категорию
+            </Button>
+            <Button
+              className={styles.button}
+              onClick={handleOpenCol}
+              type='button'
+            >
+              Добавить коллекцию
+            </Button>
+            <Button
+              className={styles.button}
+              onClick={handleOpenMat}
+              type='button'
+            >
+              Добавить материал
+            </Button>
+          </div>
+          <ItemTable
+            content={content}
+            setContent={setContent}
+            message={message}
+            setMessage={setMessage}
+            handleOpenItem={handleOpenItem}
+            setId={setId}
+          />
+          <ItemModal
+            openChange={openItem}
+            setOpenChange={setOpenItem}
+            id={id}
+            setContent={setContent}
+            open={open}
+            setOpen={setOpen}
+            message={message}
+            setMessage={setMessage}
+          />
+          <MatModal
+            openChange={openMat}
+            setOpenChange={setOpenMat}
+            open={open}
+            setOpen={setOpen}
+            message={message}
+            setMessage={setMessage}
+          />
+          <CatModal
+            openChange={openCat}
+            setOpenChange={setOpenCat}
+            open={open}
+            setOpen={setOpen}
+            message={message}
+            setMessage={setMessage}
+          />
+          <ColModal
+            openChange={openCol}
+            setOpenChange={setOpenCol}
+            open={open}
+            setOpen={setOpen}
+            message={message}
+            setMessage={setMessage}
+          />
+          <InfoModal info={message} open={open} setOpen={setOpen} />
+        </>
+      )}
     </>
   );
 }
