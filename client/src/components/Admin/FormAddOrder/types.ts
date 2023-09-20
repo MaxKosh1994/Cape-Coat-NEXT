3454b186da06d810849f544ed07894db02c8f7a7
Max Kosheutov <maxkosh1994@cmail.com>
2023-09-20 12:33:00 +0300

    sorry for this shit

---
+++ client/src/components/Admin/FormAddOrder/types.ts
@@ -0,0 +1,12 @@
+export interface IParamsFormData {
+  height: string;
+  length: string;
+  sleeve: string;
+  bust: string;
+  waist: string;
+  hips: string;
+  saddle: string;
+  loops: boolean;
+  buttons: string;
+  lining: string;
+}
